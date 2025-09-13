04f703fd3947c2ce01beaf66bab6fa7ee221920b
imad <ennouaimimad@gmail.com>
2025-09-13 16:44:11 +0200

    Init ui library

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  important: '.ui',
+  prefix: 'ui-',
+  content: [
+    "./src/**/*.{ts,tsx}"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
