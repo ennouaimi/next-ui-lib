6716cab9404f0900225728126db21135646e5a91
imad <ennouaimimad@gmail.com>
2025-09-13 17:00:38 +0200

    Init shadcn

---
+++ src/components/ui/button.stories.tsx
@@ -0,0 +1,19 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import { Button } from "./button";
+
+const meta = {
+  title: "Example/Button",
+  component: Button,
+  tags: ["docsPage"],
+} satisfies Meta<typeof Button>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Primary: Story = {
+  args: {
+    children: "Click me",
+    variant: "default",
+    size: "default",
+  },
+};
