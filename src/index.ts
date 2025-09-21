3e7575014ee005251bd02469ef829936295eca3c
imad <ennouaimimad@gmail.com>
2025-09-21 13:54:23 +0200

    export member

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export * from "./components/button/button";
 export * from "./components/card/card";
 export * from "./components/toggle/toggle";
+export * from "./components/progressBar/progressBar";
