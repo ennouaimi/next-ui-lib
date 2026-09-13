2a39de526225924f319e36222af586c4bb4abf9b
Imad Ennouaimi <45298051+ennouaimi@users.noreply.github.com>
2026-09-13 13:35:49 +0200

    fix: correct leaderboard import casing

--- src/components/leaderboard/leaderBoard.stories.tsx
+++ src/components/leaderboard/leaderBoard.stories.tsx
@@ -1,5 +1,5 @@
 import type { Meta, StoryObj } from "@storybook/react";
-import { LeaderBoard, Profile } from "./leaderBoard";
+import { LeaderBoard, Profile } from "./leaderboard";
 
 const dummyProfiles: Profile[] = [
   {
